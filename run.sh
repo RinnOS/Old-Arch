c21649d187ee45c367b9f9ac76f89cd30aef22b1
unknown <rinnray@gliroid.com>
2022-07-05 06:00:07 +0200

    Updated Scripts

--- run.sh
+++ run.sh
@@ -11,7 +11,8 @@ while $loopScript; do
     echo "3. Install AUR packages"
     echo "4. Install AppImages"
     echo "5. Run post-installation script"
-    echo "6. Exit"
+    echo "6. Run all"
+    echo "7. Exit"
     echo
     read -p "Enter your choice: " choice
 
@@ -40,8 +41,22 @@ while $loopScript; do
         sh 9-post-setup.sh || exit 1
     fi
 
-    # If they chose to exit, exit the loop.
+    # If they chose to run all, run the base-packages.sh script, then the software-packages.sh script, then the aur-packages.sh script, then the appimage-packages.sh script.
     if [ $choice -eq 6 ]; then
+        # Ask the user if they're sure they want to run all.
+        echo "Are you sure you want to run all? (y/n)"
+        read sure
+        if [ $sure == "y" ]; then
+            sh 1-base.sh || exit 1
+            sh 2-software-pacman.sh || exit 1
+            sh 3-software-aur.sh || exit 1
+            sh 4-software-appimage.sh || exit 1
+            sh 9-post-setup.sh || exit 1
+        fi
+    fi
+
+    # If they chose to exit, exit the loop.
+    if [ $choice -eq 7 ]; then
         loopScript=false
     fi
 done
