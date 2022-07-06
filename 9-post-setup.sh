ddb867f1b53062ebbc75b8679d949205c3bb4439
unknown <rinnray@gliroid.com>
2022-07-07 00:01:11 +0200

    Moved dotfiles location to yaml

--- 9-post-setup.sh
+++ 9-post-setup.sh
@@ -6,47 +6,47 @@ echo "##################"
 echo
 
 DOTDIR=$HOME/RinnOS/dotfiles
-DOTFILES=(
-    # --- Home ---
-    '.zshrc'
-    '.gitconfig'
-    '.emacs.d'
-    '.doom.d'
-    'wallpapers'
-    'scripts'
+# DOTFILES=(
+#     # --- Home ---
+#     '.zshrc'
+#     '.gitconfig'
+#     '.emacs.d'
+#     '.doom.d'
+#     'wallpapers'
+#     'scripts'
 
-    # --- .config ---
-    '.config/i3'
-    '.config/pulsemeeter'
-    '.config/flameshot'
-)
+#     # --- .config ---
+#     '.config/i3'
+#     '.config/pulsemeeter'
+#     '.config/flameshot'
+# )
 
 LOCALDOTDIR=/mnt/Vault/Dotfiles/Linux/Arch
-LOCALDOTFILES=(
-    # --- Home ---
-    '.ssh'
-    '.waterfox'
-    '.vscode'
+# LOCALDOTFILES=(
+#     # --- Home ---
+#     '.ssh'
+#     '.waterfox'
+#     '.vscode'
 
-    # --- .config ---
-    '.config/BetterDiscord'
-    '.config/Code'
-    '.config/discord'
-    '.config/discordcanary'
-    '.config/FreeTube'
-    '.config/Logseq'
-    '.config/lutris'
-    '.config/Rambox'
-    '.config/rambox'
-    '.config/wtwitch'
+#     # --- .config ---
+#     '.config/BetterDiscord'
+#     '.config/Code'
+#     '.config/discord'
+#     '.config/discordcanary'
+#     '.config/FreeTube'
+#     '.config/Logseq'
+#     '.config/lutris'
+#     '.config/Rambox'
+#     '.config/rambox'
+#     '.config/wtwitch'
 
-    # --- .local ---
-    '.local/share/lutris'
-    '.local/share/Steam'
-    '.local/share/vulkan'
-    '.local/share/PolyMC'
-    '.local/share/7DaysToDie'
-)
+#     # --- .local ---
+#     '.local/share/lutris'
+#     '.local/share/Steam'
+#     '.local/share/vulkan'
+#     '.local/share/PolyMC'
+#     '.local/share/7DaysToDie'
+# )
 
 echo "-----------------------"
 echo "-- Starting Services --"
@@ -73,19 +73,41 @@ echo "-- Dealing with dotfiles --"
 echo "---------------------------"
 echo
 
-for dotfile in "${DOTFILES[@]}"; do
+I=0
+while [ $I -le $(yq '.git | length' dotfiles.yaml) ]; do
+    dotfile=$(yq .git[$I] dotfiles.yaml)
+
     rm -rf "${HOME}/$dotfile"
     echo
     echo "Linking ${DOTDIR}/${dotfile} to ${HOME}/${dotfile}"
     cp -faTs "${DOTDIR}/${dotfile}" "${HOME}/${dotfile}"
+    ((I++))
 done
 
-for localdotfile in "${LOCALDOTFILES[@]}"; do
+# for dotfile in "${DOTFILES[@]}"; do
+#     rm -rf "${HOME}/$dotfile"
+#     echo
+#     echo "Linking ${DOTDIR}/${dotfile} to ${HOME}/${dotfile}"
+#     cp -faTs "${DOTDIR}/${dotfile}" "${HOME}/${dotfile}"
+# done
+
+I=0
+while [ $I -le $(yq '.local | length' dotfiles.yaml) ]; do
+    localdotfile=$(yq .local[$I] dotfiles.yaml)
+
     rm -rf "${HOME}/${localdotfile}"
     echo
     echo "Linking ${LOCALDOTDIR}/${localdotfile} to ${HOME}/${localdotfile}"
     cp -faTs "${LOCALDOTDIR}/${localdotfile}" "${HOME}/${localdotfile}"
+    ((I++))
 done
+
+# for localdotfile in "${LOCALDOTFILES[@]}"; do
+#     rm -rf "${HOME}/${localdotfile}"
+#     echo
+#     echo "Linking ${LOCALDOTDIR}/${localdotfile} to ${HOME}/${localdotfile}"
+#     cp -faTs "${LOCALDOTDIR}/${localdotfile}" "${HOME}/${localdotfile}"
+# done
 
 echo "-------------------------"
 echo "-- Some extra stuff... --"
