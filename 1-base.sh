a3e1d238cf1e657f3b5de53e70b0e8b29f616f36
unknown <rinnray@gliroid.com>
2022-07-14 00:52:42 +0200

    Drives gets added to fstab

--- 1-base.sh
+++ 1-base.sh
@@ -16,8 +16,28 @@ while [ $I -le $(yq '.base | length' packages.yaml) ]; do
     ((I++))
 done
 
-echo "Mounting drives"
-sh ./dotfiles/scripts/mountDrives.sh
+I=0
+while [ $I -le $(yq '.drives | length' config.yaml) ]; do
+    DRIVE=$(yq .drives[$I] config.yaml)
+
+    if [ ! -d "/mnt/$DRIVE" ]; then
+        echo "Creating folder /mnt/$DRIVE"
+        sudo mkdir "/mnt/$DRIVE"
+    fi
+
+    sudo echo LABEL=$DRIVE /mnt/$DRIVE auto defaults 0 0 >> /etc/fstab
+
+    ((I++))
+done
+
+# sudo cat <<EOF >> /etc/fstab
+# # RinnOS
+# LABEL=$()
+
+# EOF
+
+#echo "Mounting drives"
+#sh ./dotfiles/scripts/mountDrives.sh
 
 echo "###########"
 echo "## DONE! ##"
