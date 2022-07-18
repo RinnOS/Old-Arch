bd5b610f113a031b4c1f9cfc08e608e2f703e994
Rinnray <rinnray@gliroid.com>
2022-07-18 23:15:06 +0200

    New package

--- 9-post-setup.sh
+++ 9-post-setup.sh
@@ -27,6 +27,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "--------------"
+echo "-- Vim-plug --"
+echo "--------------"
+echo
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 echo "---------------------------"
 echo "-- Dealing with dotfiles --"
 echo "---------------------------"
