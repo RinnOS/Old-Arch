7e71c4ec7b8b167564d5e136dca8d6319eb25127
Rinnray <rinnray@gliroid.com>
2022-07-25 22:27:43 +0200

    New package installation

--- run.sh
+++ run.sh
@@ -8,9 +8,10 @@ while $loopScript; do
     echo "2. Install software packages"
     echo "3. Install AUR packages"
     echo "4. Install AppImages"
-    echo "5. Run post-installation script"
-    echo "6. Run all"
-    echo "7. Exit"
+	echo "5. Install Flatpaks"
+    echo "6. Run post-installation script"
+    echo "7. Run all"
+    echo "8. Exit"
     echo
     read -p "Enter your choice: " choice
 
@@ -34,13 +35,18 @@ while $loopScript; do
         sh 4-software-appimage.sh || exit 1
     fi
 
+	# If they chose to install Flatpaks, run the flatpak-packages.sh script.
+	if [ $choice -eq 5 ]; then
+		sh 5-software-flatpak.sh || exit 1
+	fi
+
     # If they chose to run the post-installation script, run the post-setup.sh script.
-    if [ $choice -eq 5 ]; then
+    if [ $choice -eq 6 ]; then
         sh 9-post-setup.sh || exit 1
     fi
 
     # If they chose to run all, run the base-packages.sh script, then the software-packages.sh script, then the aur-packages.sh script, then the appimage-packages.sh script.
-    if [ $choice -eq 6 ]; then
+    if [ $choice -eq 7 ]; then
         # Ask the user if they're sure they want to run all.
         echo "Are you sure you want to run all? (y/n)"
         read sure
@@ -49,12 +55,13 @@ while $loopScript; do
             sh 2-software-pacman.sh || exit 1
             sh 3-software-aur.sh || exit 1
             sh 4-software-appimage.sh || exit 1
+			sh 5-software-flatpak.sh || exit 1
             sh 9-post-setup.sh || exit 1
         fi
     fi
 
     # If they chose to exit, exit the loop.
-    if [ $choice -eq 7 ]; then
+    if [ $choice -eq 8 ]; then
         loopScript=false
     fi
 done
